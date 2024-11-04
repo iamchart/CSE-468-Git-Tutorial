972eabf02daebb3d2f4df79ec9a076907dea72bb
iamchart <144873834+iamchart@users.noreply.github.com>
2024-11-03 17:47:09 -0700

    Created pilot class that extends person
    Meaning that pilot now holds all the same parameters that person has and
    adds on two more parameters
    - license_no (int)
    - flight_hours (int)

--- test_class.py
+++ test_class.py
@@ -6,3 +6,12 @@ class Person:
 
     def __str__(self):
         return f"{self.name} ({self.age}) - Has {self.cars} car{'s' if self.cars != 1 else ''}"
+
+class Pilot(Person):
+    def __init__(self, name, age, cars, license_no, flight_hours):
+        super().__init__(name, age, cars)
+        self.license_no = license_no
+        self.flight_hours = flight_hours
+    
+    def __str__(self):
+        return f"{self.name} ({self.age}) [License No. {self.license_no}] has {self.flight_hours} hours of flight"
