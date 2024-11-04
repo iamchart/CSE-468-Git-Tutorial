7734984224bf0dd7ecbef738205d92801e6e6d61
iamchart <144873834+iamchart@users.noreply.github.com>
2024-11-03 17:27:18 -0700

    Added new cars field and implemented it
    Now cars will be an extra parameter that is required by the person
    class, totaling the parameters at:
    - name (str)
    - age (int)
    - cars (int)

--- implement.py
+++ implement.py
@@ -2,5 +2,5 @@ from test_class import Person
 import random
 
 for i in range(10):
-    person = Person(f'Person {i+1}', random.randrange(13, 79))
+    person = Person(f'Person {i+1}', random.randrange(13, 79), random.randrange(0, 10))
     print(person)
--- test_class.py
+++ test_class.py
@@ -1,7 +1,8 @@
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age, cars):
         self.name = name
         self.age = age
+        self.cars = cars
 
     def __str__(self):
-        return f"{self.name} ({self.age})"
+        return f"{self.name} ({self.age}) - Has {self.cars} car{'s' if self.cars != 1 else ''}"
