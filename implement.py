1036000e8afc101612d183b5315f77518c2c31ce
iamchart <144873834+iamchart@users.noreply.github.com>
2024-11-03 17:48:02 -0700

    Implemented arr of persons and pilots
    Now implement.py will print the persons and pilots initialized

--- implement.py
+++ implement.py
@@ -1,6 +1,16 @@
-from test_class import Person
+from test_class import Person, Pilot
 import random
 
-for i in range(10):
-    person = Person(f'Person {i+1}', random.randrange(13, 79), random.randrange(0, 10))
-    print(person)
+# for i in range(10):
+#     person = Person(f'Person {i+1}', random.randrange(13, 79), random.randrange(0, 10))
+#     print(person)
+
+person1 = Person("Alice", 30, 2)
+person2 = Person("Bob", 25, 1)
+pilot1 = Pilot("Charlie", 40, 1, "XY123", 2000)
+pilot2 = Pilot("Dana", 34, 0, "YZ456", 3500)
+
+arr = [person1, person2, pilot1, pilot2]
+
+for i in arr:
+    print(i)
